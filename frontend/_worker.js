3055a11e08d68fc4e0ba643ed1b3117197c72af0
liqingyilove-cmd <liqingyilove@gmail.com>
2026-07-17 10:52:12 +0800

    Delete frontend/_worker.js

--- frontend/_worker.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    return new Response("Hello from TVBox aggregator! Worker is alive.", {
-        headers: { "Content-Type": "text/plain;charset=UTF-8" }
-    });
-}
